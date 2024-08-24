285ee527c07ca113a72c9ba970e630855d0868e4
AyushGlitch <ayusharyansingh3@gmail.com>
2024-08-24 22:35:50 +0530

    added circles

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="dark">
-            <body className={inter.className}>
+            <body className={inter.className} style={{position: "relative", overflowX: "hidden"}}>
                 <Navbar />
+                    <div className="absolute bg-red-500 h-80 w-80 rounded-full left-1/3 top-1/3 mix-blend-exclusion -z-10 filter blur-xl opacity-70 animate-blob "></div>
+                    <div className="absolute bg-cyan-500 h-80 w-80 rounded-full right-1/3 top-1/3 mix-blend-exclusion -z-10 filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+                    <div className="absolute bg-emerald-500 h-80 w-80 rounded-full left-[40%] top-1/2 mix-blend-exclusion -z-10 filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
                 {children}
             </body>
         </html>
